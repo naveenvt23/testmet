dab96d9076570c3ac167a5aa44e08d7ffd3b2912
unknown <sairamreddy9866@gmail.com>
2020-10-03 10:42:19 +0530

    added text,php

---
+++ text.php
@@ -0,0 +1,8 @@
+<?php
+require_once "twilio/vendor/autoload.php";
+require_once "twilio/twilio-php-main/src/Twilio/autoload.php";
+use Twilio\Rest\Client;
+function SendWhatsappMessage($entityData){
+	$adb = PearDatabase::getInstance();
+	$wsId   	= $entityData->get('assigned_user_id');
+	$parts 		= explode('x', $wsId);
